c1da2f41c6c9fffb7a72a816139e71f0c4ee41aa
Hero Solutions <info@herosolutions.be>
2023-03-16 17:14:59 +0100

    Removes unneeded rights

--- src/Command/GenerateIIIFManifestsCommand.php
+++ src/Command/GenerateIIIFManifestsCommand.php
@@ -521,20 +521,28 @@ class GenerateIIIFManifestsCommand extends Command implements ContainerAwareInte
 
             $rightsSource = '';
             $rightsSourceLC = '';
-            $buttonURL = '';
             if(array_key_exists($this->rightsSourceV3, $rsData)) {
                 $rightsSource = $rsData[$this->rightsSourceV3];
                 $rightsSourceLC = strtolower($rightsSource);
                 if(strpos($rightsSourceLC, 'http://creativecommons.org/publicdomain/mark/1.0/') !== false || $rightsSourceLC === 'public domain / cc-pdm') {
+                    if($rightsSourceLC === 'http://creativecommons.org/publicdomain/mark/1.0/' || $rightsSourceLC === 'public domain / cc-pdm') {
+                        $rightsSource = '';
+                    }
                     $rights = 'https://creativecommons.org/publicdomain/mark/1.0/';
                     $buttonURL = '<a href="http://creativecommons.org/publicdomain/mark/1.0/"><img src="https://licensebuttons.net/p/mark/1.0/88x31.png"/></a>';
                 } else if(strpos($rightsSourceLC, 'http://creativecommons.org/publicdomain/zero/1.0/') !== false || $rightsSourceLC === 'cc0') {
+                    if($rightsSourceLC === 'http://creativecommons.org/publicdomain/zero/1.0/' || $rightsSourceLC === 'cc0') {
+                        $rightsSource = '';
+                    }
                     $rights = 'https://creativecommons.org/publicdomain/zero/1.0/';
                     $buttonURL = '<a href="https://creativecommons.org/publicdomain/zero/1.0/"><img src="https://licensebuttons.net/p/zero/1.0/88x31.png"/></a>';
                 } else if(strpos($rightsSourceLC, 'sabam') !== false || strpos($rightsSourceLC, '©') !== false) {
                     $rights = 'https://rightsstatements.org/vocab/InC/1.0/';
                     $buttonURL = '<a href="https://rightsstatements.org/vocab/InC/1.0/"><img src="https://rightsstatements.org/files/buttons/InC.dark-white-interior.png"/></a>';
                 } else if(strpos($rightsSourceLC, 'public domain') !== false || strpos($rightsSourceLC, 'publiek domein') !== false) {
+                    if($rightsSourceLC === 'public domain' || $rightsSourceLC === 'publiek domein') {
+                        $rightsSource = '';
+                    }
                     $rights = 'https://creativecommons.org/publicdomain/mark/1.0/';
                     $buttonURL = '<a href="http://creativecommons.org/publicdomain/mark/1.0/"><img src="https://licensebuttons.net/p/mark/1.0/88x31.png"/></a>';
                 } else {
