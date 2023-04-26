4ff13a5b1989089631b4b94cf1df9ada272b0dcf
Hero Solutions <info@herosolutions.be>
2023-04-26 18:42:28 +0200

    Adds debugging

--- src/Command/FtpToResourceSpaceCommand.php
+++ src/Command/FtpToResourceSpaceCommand.php
@@ -68,8 +68,9 @@ class FtpToResourceSpaceCommand extends Command implements ContainerAwareInterfa
         echo 'Test 0' . $file . PHP_EOL;
 
         Imagick::setResourceLimit(imagick::RESOURCETYPE_MEMORY, 512000000);
-        Imagick::setResourceLimit(imagick::RESOURCETYPE_DISK, 50000000000);
-        Imagick::setResourceLimit(imagick::RESOURCETYPE_FILE, 50000000000);
+        Imagick::setResourceLimit(imagick::RESOURCETYPE_MAP, 512000000);
+        Imagick::setResourceLimit(imagick::RESOURCETYPE_DISK, 5000000000);
+        Imagick::setResourceLimit(imagick::RESOURCETYPE_FILE, 5000000000);
         Imagick::setRegistry('temporary-path', '/mnt/volume_ams3_01/');
 
         echo 'Imagick res 1 ' . Imagick::getResourceLimit(imagick::RESOURCETYPE_AREA);
