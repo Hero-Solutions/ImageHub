8366a3ddfa154c281a2de2e088a5bbc8c8f0b757
Hero Solutions <info@herosolutions.be>
2023-07-12 22:02:09 +0200

    Debugs signature

--- src/Utils/FastImageSizeImpl.php
+++ src/Utils/FastImageSizeImpl.php
@@ -6,38 +6,13 @@ use FastImageSize\FastImageSize;
 
 class FastImageSizeImpl extends FastImageSize
 {
-    /**
-     * Get dimensions of image if type is unknown
-     *
-     * @param string $filename Path to file
-     */
-    protected function getImagesizeUnknownType($filename)
-    {
-        echo 'Unknown type.' . PHP_EOL;
-        // Grab the maximum amount of bytes we might need
-        $data = $this->getImage($filename, 0, Type\TypeJpeg::JPEG_MAX_HEADER_SIZE, false);
-
-        if ($data !== false)
-        {
-            $this->loadAllTypes();
-            foreach ($this->type as $imageType)
-            {
-                $imageType->getSize($filename);
-
-                if (sizeof($this->size) > 1)
-                {
-                    break;
-                }
-            }
-        }
-    }
-
     protected function getImageSizeByExtension($file, $extension)
     {
         echo 'Okay' . PHP_EOL;
         $extension = strtolower($extension);
         $this->loadExtension($extension);
         if (isset($this->classMap[$extension])) {
+            echo 'OKAY ' . PHP_EOL;
             $impl = new TypeTifImpl($this);
             $impl->getSize($file);
         } else {
