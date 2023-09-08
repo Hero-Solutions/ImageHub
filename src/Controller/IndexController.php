965d07fb7094225372bde5021615f8bc89364fc7
Hero Solutions <info@herosolutions.be>
2023-09-08 10:10:32 +0200

    Adds index page and pages with more informatino

---
+++ src/Controller/IndexController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\IIIfManifest;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\KernelInterface;
+use Symfony\Component\Routing\Annotation\Route;
+
+class IndexController extends AbstractController
+{
+    /**
+     * @Route("/", name="index")
+     */
+    public function index(Request $request, KernelInterface $kernel)
+    {
+        // Make sure the service URL name ends with a trailing slash
+        $baseUrl = rtrim($this->getParameter('service_url'), '/') . '/';
+        $mainIiifVersion = $this->getParameter('main_iiif_version');
+
+        $ids = $this->container->get('doctrine')
+            ->getManager()
+            ->createQueryBuilder()
+            ->select('COUNT(m.id) AS c')
+            ->from(IIIfManifest::class, 'm')
+            ->where('m.manifestId LIKE :id')
+            ->setParameter('id', $baseUrl . $mainIiifVersion . '/%/manifest.json')
+            ->getQuery()
+            ->getResult();
+        if(count($ids) > 0) {
+            return new Response($ids[0]['c'], 200);
+        } else {
+            return new Response(0, 404);
+        }
+    }
+}
