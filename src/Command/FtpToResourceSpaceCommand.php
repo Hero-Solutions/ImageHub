c17e18c87f3c6a9c22e4aa022c5951ee3db4f923
Hero Solutions <info@herosolutions.be>
2023-10-10 10:10:01 +0200

    Enables bigtiff support

--- src/Command/FtpToResourceSpaceCommand.php
+++ src/Command/FtpToResourceSpaceCommand.php
@@ -101,7 +101,7 @@ class FtpToResourceSpaceCommand extends Command implements ContainerAwareInterfa
 
         try {
             $exifToolOutput = [];
-            exec('exiftool ' . $file, $exifToolOutput);
+            exec('exiftool -api largefilesupport=1 ' . $file, $exifToolOutput);
             $hasWidth = false;
             $hasHeight = false;
             foreach($exifToolOutput as $line) {
