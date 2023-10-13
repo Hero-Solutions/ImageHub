e098f158ede78d6b7260b8fe538ad877786d893f
Hero Solutions <info@herosolutions.be>
2023-10-13 18:03:02 +0200

    Sets correct table

--- src/Entity/Transcription.php
+++ src/Entity/Transcription.php
@@ -6,7 +6,7 @@ use Doctrine\ORM\Mapping as ORM;
 
 /**
  * @ORM\Entity(repositoryClass="App\Repository\TranscriptionRepository")
- * @ORM\Table(name="annotation")
+ * @ORM\Table(name="transcription")
  */
 class Transcription
 {
