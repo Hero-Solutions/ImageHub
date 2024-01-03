4f85005f9123cd55164cb5a4e0f1f14eff201d52
Hero Solutions <info@herosolutions.be>
2024-01-03 22:25:33 +0100

    Readds empty repositories

--- src/Repository/ResourceDataRepository.php
+++ src/Repository/ResourceDataRepository.php
@@ -2,14 +2,8 @@
 
 namespace App\Repository;
 
-use App\Entity\ResourceData;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
-use Doctrine\Common\Persistence\ManagerRegistry;
 
 class ResourceDataRepository extends ServiceEntityRepository
 {
-    public function __construct(ManagerRegistry $registry)
-    {
-        parent::__construct($registry, ResourceData::class);
-    }
 }
