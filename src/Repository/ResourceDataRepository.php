9b91cb2068e950222373516cc47707caadc23a17
Hero Solutions <info@herosolutions.be>
2024-01-03 22:28:03 +0100

    Readds repositories with constructor matching super

--- src/Repository/ResourceDataRepository.php
+++ src/Repository/ResourceDataRepository.php
@@ -2,8 +2,14 @@
 
 namespace App\Repository;
 
+use App\Entity\ResourceData;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\Persistence\ManagerRegistry;
 
 class ResourceDataRepository extends ServiceEntityRepository
 {
+    public function __construct(ManagerRegistry $registry)
+    {
+        parent::__construct($registry, ResourceData::class);
+    }
 }
--- src/Repository/TranscriptionRepository.php
+++ src/Repository/TranscriptionRepository.php
@@ -2,8 +2,49 @@
 
 namespace App\Repository;
 
+use App\Entity\Transcription;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\Persistence\ManagerRegistry;
 
+/**
+ * @method Transcription|null find($id, $lockMode = null, $lockVersion = null)
+ * @method Transcription|null findOneBy(array $criteria, array $orderBy = null)
+ * @method Transcription[]    findAll()
+ * @method Transcription[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
+ */
 class TranscriptionRepository extends ServiceEntityRepository
 {
+    public function __construct(ManagerRegistry $registry)
+    {
+        parent::__construct($registry, Transcription::class);
+    }
+
+    // /**
+    //  * @return Transcription[] Returns an array of Transcription objects
+    //  */
+    /*
+    public function findByExampleField($value)
+    {
+        return $this->createQueryBuilder('i')
+            ->andWhere('i.exampleField = :val')
+            ->setParameter('val', $value)
+            ->orderBy('i.id', 'ASC')
+            ->setMaxResults(10)
+            ->getQuery()
+            ->getResult()
+        ;
+    }
+    */
+
+    /*
+    public function findOneBySomeField($value): ?Transcription
+    {
+        return $this->createQueryBuilder('i')
+            ->andWhere('i.exampleField = :val')
+            ->setParameter('val', $value)
+            ->getQuery()
+            ->getOneOrNullResult()
+        ;
+    }
+    */
 }
