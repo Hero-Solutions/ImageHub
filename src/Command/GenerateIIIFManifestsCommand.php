d2c915333540ca93bfb8491237b5630c00a549cf
Hero Solutions <info@herosolutions.be>
2024-03-18 22:22:00 +0100

    Conforms to the presentation API

--- src/Command/GenerateIIIFManifestsCommand.php
+++ src/Command/GenerateIIIFManifestsCommand.php
@@ -1288,7 +1288,7 @@ class GenerateIIIFManifestsCommand extends Command implements ContainerAwareInte
                 $manifests[] = array(
                     'id' => $manifestId,
                     'type' => 'Manifest',
-                    'label' => [ $manifestLabel ]
+                    'label' => $manifestLabel
                 );
 
                 if($storeInLido) {
